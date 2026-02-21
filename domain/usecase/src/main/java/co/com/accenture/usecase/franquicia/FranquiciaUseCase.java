cd25b72cbf54407c2ea523918f453f7f6581d4d6
Johs Salinas <johssalinas2work@gmail.com>
2026-02-21 16:07:41 -0500

    feat(franquicias): implementar métodos CRUD en FranquiciaUseCase

--- domain/usecase/src/main/java/co/com/accenture/usecase/franquicia/FranquiciaUseCase.java
+++ domain/usecase/src/main/java/co/com/accenture/usecase/franquicia/FranquiciaUseCase.java
@@ -1,6 +1,37 @@
 package co.com.accenture.usecase.franquicia;
 
+import co.com.accenture.model.franquicia.Franquicia;
+import co.com.accenture.model.franquicia.gateways.FranquiciaRepository;
 import lombok.RequiredArgsConstructor;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+
 @RequiredArgsConstructor
 public class FranquiciaUseCase {
+
+    private final FranquiciaRepository repository;
+
+    public Mono<Franquicia> save(Franquicia franquicia) {
+        return repository.save(franquicia);
+    }
+
+    public Mono<Franquicia> findById(String id) {
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Franquicia not found!")));
+    }
+
+    public Flux<Franquicia> findAll() {
+        return repository.findAll();
+    }
+
+    public Mono<Void> deleteById(String id) {
+        return repository.deleteById(id);
+    }
+
+    public Mono<Franquicia> updateName(String id, String newName) {
+        return repository.findById(id)
+                .switchIfEmpty(Mono.error(new RuntimeException("Not found")))
+                .map(franquiciaExistente -> franquiciaExistente.toBuilder().name(newName).build())
+                .flatMap(repository::save);
+    }
 }
