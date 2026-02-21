26c3a063d28a7ffe285bbb968209638d1b67dabd
Johs Salinas <johssalinas2work@gmail.com>
2026-02-21 06:24:52 -0500

    feat(config): Se inicia el proyecto tomando como base la arquitectura Scaffold Clean Architecture de Bancolombia

---
+++ applications/app-service/src/main/java/co/com/accenture/config/UseCasesConfig.java
@@ -0,0 +1,14 @@
+package co.com.accenture.config;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.FilterType;
+
+@Configuration
+@ComponentScan(basePackages = "co.com.accenture.usecase",
+        includeFilters = {
+                @ComponentScan.Filter(type = FilterType.REGEX, pattern = "^.+UseCase$")
+        },
+        useDefaultFilters = false)
+public class UseCasesConfig {
+}
