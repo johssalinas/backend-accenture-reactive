f1d64a02cd57b7d00da9bf03b65553fc9e049498
Johs Salinas <johssalinas2work@gmail.com>
2026-02-23 10:18:23 -0500

    style: Formatear la definición del servicio ECS para mayor claridad

--- terraform/main.tf
+++ terraform/main.tf
@@ -556,11 +556,11 @@ resource "aws_ecs_task_definition" "app" {
 }
 
 resource "aws_ecs_service" "app" {
-  name            = "${var.project_name}-service"
-  cluster         = aws_ecs_cluster.main.id
-  task_definition = aws_ecs_task_definition.app.arn
-  desired_count   = var.ecs_desired_count
-  launch_type     = "FARGATE"
+  name                              = "${var.project_name}-service"
+  cluster                           = aws_ecs_cluster.main.id
+  task_definition                   = aws_ecs_task_definition.app.arn
+  desired_count                     = var.ecs_desired_count
+  launch_type                       = "FARGATE"
   health_check_grace_period_seconds = 120
 
   network_configuration {
